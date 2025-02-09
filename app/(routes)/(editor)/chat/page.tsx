5e7077c965c18ade3fc3141e658c95a6b9a2bb6d
Oscar <oscarrc.web@gmail.com>
2025-02-09 20:53:11 +0000

    feat: add chat initial page

--- app/(routes)/(editor)/chat/page.tsx
+++ app/(routes)/(editor)/chat/page.tsx
@@ -1,5 +1,23 @@
+import ButtonSquare from "@/app/_components/ButtonSquare";
+import { VscSend } from "react-icons/vsc";
+
 const ChatTab = () => {
-  return <section>Chat</section>;
+  return (
+    <section className="flex flex-col flex-1 justify-center">
+      <div className="flex flex-col items-center gap-8">
+        <h2 className="text-center text-4xl font-bold px-4">Ask any question about your notes</h2>
+        <label className="flex input input-lg w-full max-w-2xl bg-base-200 items-center pr-2">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1" />
+          <ButtonSquare className="btn btn-circle btn-md btn-neutral">
+            <VscSend className="h-6 w-6" />
+          </ButtonSquare>
+        </label>
+      </div>
+    </section>
+  );
 };
 
 export default ChatTab;
--- app/(routes)/(editor)/page.tsx
+++ app/(routes)/(editor)/page.tsx
@@ -13,7 +13,7 @@ const NewTab = () => {
   return (
     <section className='flex h-full items-center justify-center p-8'>
       <div className='align-center flex max-w-md flex-col gap-16'>
-        <h2 className='justify-between text-center text-4xl font-bold'>
+        <h2 className='text-center text-4xl font-bold'>
           No file is open
         </h2>
         <div className='col-gap-4 grid grid-cols-2 items-center'>
