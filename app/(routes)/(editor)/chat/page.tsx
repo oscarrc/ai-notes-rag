14d2b0f576a115251b9f930f0af29d4be73e95cb
Oscar <oscarrc.web@gmail.com>
2025-02-10 12:59:54 +0000

    fix: make chat input a textarea

--- app/(routes)/(editor)/chat/page.tsx
+++ app/(routes)/(editor)/chat/page.tsx
@@ -6,12 +6,11 @@ const ChatTab = () => {
     <section className="flex flex-col flex-1 justify-center p-8">
       <div className="flex flex-col items-center gap-8">
         <h2 className="text-center text-4xl font-bold px-4">Ask any question about your notes</h2>
-        <label className="flex input input-lg w-full max-w-2xl bg-base-200 items-center pr-2">
-          <input
-            type="text"
+        <label className="flex textarea w-full max-w-2xl bg-base-200 items-center pr-2 gap-4">
+          <textarea
             placeholder="Send a message"
-            className="flex-1" />
-          <ButtonSquare className="btn btn-circle btn-md btn-neutral">
+            className="flex-1 bg-transparent  focus-visible:outline-none h-24" />
+          <ButtonSquare size="md" className="btn-neutral self-end">
             <VscSend className="h-6 w-6" />
           </ButtonSquare>
         </label>
