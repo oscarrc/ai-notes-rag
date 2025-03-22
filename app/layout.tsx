7464294ba6f93baa63ab88dd7812c0f65fcef226
Oscar <oscarrc.web@gmail.com>
2025-03-22 11:21:28 +0100

    feat(indicators): add toast context to show toasts  with information

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,12 @@ import QueryProvider from '@/app/_providers/QueryProvider';
 import Search from '@/app/_components/Search';
 import Settings from '@/app/_components/Settings';
 import Shortcuts from '@/app/_components/Shortcuts';
+import { ToastProvider } from './_providers/ToastProvider';
 
 interface EditorLayoutProps extends LayoutProps {
   sidebar: React.ReactNode;
   tabbar: React.ReactNode;
+  toast: React.ReactNode;
 }
 
 export const metadata: Metadata = {
@@ -17,26 +19,34 @@ export const metadata: Metadata = {
   description: 'RAG-based AI powered knowledge base',
 };
 
-const EditorLayout = ({ children, sidebar, tabbar }: EditorLayoutProps) => {
+const EditorLayout = ({
+  children,
+  sidebar,
+  tabbar,
+  toast,
+}: EditorLayoutProps) => {
   return (
     <html lang='en'>
       <body>
-        <AiProvider>
-          <QueryProvider>
-            <div className='grid-max-auto grid bg-base-100'>
-              {sidebar}
-              <div className='grid-nav-auto grid h-dvh'>
-                {tabbar}
-                <main className='flex max-h-dvh w-full flex-col overflow-y-auto'>
-                  {children}
-                  <Shortcuts />
-                  <Settings />
-                  <Search />
-                </main>
+        <ToastProvider>
+          <AiProvider>
+            <QueryProvider>
+              <div className='grid-max-auto grid bg-base-100'>
+                {sidebar}
+                <div className='grid-nav-auto grid h-dvh'>
+                  {tabbar}
+                  <main className='flex max-h-dvh w-full flex-col overflow-y-auto'>
+                    {children}
+                    <Shortcuts />
+                    <Settings />
+                    <Search />
+                  </main>
+                </div>
+                {toast}
               </div>
-            </div>
-          </QueryProvider>
-        </AiProvider>
+            </QueryProvider>
+          </AiProvider>
+        </ToastProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,57 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+
 import { IS_APPLE } from '@/app/_utils/shortcuts';
 import { chatTab } from '@/app/_utils/tabs';
 import { getFilePath } from '@/app/_utils/files';
 import { showModal } from '@/app/_utils/modals';
-import { useEffect } from 'react';
+import { useAi } from './_hooks/useAi';
 import { useFilesQuery } from '@/app/_hooks/useFilesQuery';
 import useNavigationStore from '@/app/_store/navigationStore';
+import { useToast } from './_hooks/useToast';
 
 const NewTab = () => {
   const { addTab, selectedNode } = useNavigationStore();
   const { createFile } = useFilesQuery();
+  const { showToast, updateToast } = useToast();
+  const { embeddingProgress, generationProgress } = useAi();
+  const embeddingToast = useRef<string | null>(null);
+  const generationToast = useRef<string | null>(null);
 
   useEffect(() => {
     navigator.serviceWorker.register(
       new URL('./_workers/serviceWorker.ts', import.meta.url),
       { scope: '/' }
     );
+
+    embeddingToast.current = showToast({
+      message: 'Loading embeddings model',
+      type: 'info',
+      duration: -1,
+      progress: embeddingProgress,
+    });
+
+    generationToast.current = showToast({
+      message: 'Loading generation model',
+      type: 'info',
+      duration: -1,
+      progress: generationProgress,
+    });
+
+    showToast({
+      message: 'timed toast',
+      type: 'error',
+      duration: 5000,
+    });
   }, []);
+
+  useEffect(() => {
+    generationToast?.current &&
+      updateToast(generationToast.current, { progress: generationProgress });
+    embeddingToast?.current &&
+      updateToast(embeddingToast.current, { progress: embeddingProgress });
+  }, [embeddingProgress, generationProgress]);
 
   return (
     <section className='flex h-full items-center justify-center p-8'>
