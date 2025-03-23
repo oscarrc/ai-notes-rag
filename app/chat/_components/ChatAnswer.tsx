3af9abf44766bc508db23980e1ee41cdcbcbd3d0
Oscar <oscarrc.web@gmail.com>
2025-03-23 13:41:37 +0100

    feat(rag-optimization): save answer as note

--- app/chat/_components/ChatAnswer.tsx
+++ app/chat/_components/ChatAnswer.tsx
@@ -3,6 +3,9 @@ import { VscCopy, VscRefresh, VscSave } from 'react-icons/vsc';
 import ButtonSquare from '@/app/_components/ButtonSquare';
 import ChatSource from './ChatSource';
 import ReactMarkdown from 'react-markdown';
+import { getFilePath } from '@/app/_utils/files';
+import { useFilesQuery } from '@/app/_hooks/useFilesQuery';
+import useNavigationStore from '@/app/_store/navigationStore';
 import { useToast } from '@/app/_hooks/useToast';
 
 interface ChatAnswerProps {
@@ -19,6 +22,7 @@ const ChatAnswer = ({
   sources = [],
 }: ChatAnswerProps) => {
   const { showToast } = useToast();
+  const { createFile } = useFilesQuery();
 
   const copyToClipboard = () => {
     navigator.clipboard
@@ -37,6 +41,33 @@ const ChatAnswer = ({
           duration: 3000,
         });
       });
+  };
+
+  const createNote = async () => {
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+
+    const newFile: FileNode = {
+      name: `answer-${timestamp}`,
+      path: `${getFilePath(null)}/answers`,
+      extension: '.md',
+      content: text,
+    };
+
+    try {
+      await createFile(newFile);
+
+      showToast({
+        message: 'Note created and opened in new tab',
+        type: 'success',
+        duration: 3000,
+      });
+    } catch {
+      showToast({
+        message: 'Failed to save answer as note',
+        type: 'error',
+        duration: 3000,
+      });
+    }
   };
   return text.length ? (
     <div className='flex w-full max-w-2xl flex-col gap-4'>
@@ -61,7 +92,11 @@ const ChatAnswer = ({
           >
             <VscCopy className='h-4 w-4' />
           </ButtonSquare>
-          <ButtonSquare size='xs' className='opacity-75 hover:opacity-100'>
+          <ButtonSquare
+            size='xs'
+            className='opacity-75 hover:opacity-100'
+            onClick={createNote}
+          >
             <VscSave className='h-4 w-4' />
           </ButtonSquare>
           <ButtonSquare size='xs' className='opacity-75 hover:opacity-100'>
