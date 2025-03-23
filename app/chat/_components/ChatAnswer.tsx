ef156c91296c6a06d77f59a7eb696cbd8911037b
Oscar <oscarrc.web@gmail.com>
2025-03-23 13:04:22 +0100

    feat(rag-optimization): render answers as markdown

--- app/chat/_components/ChatAnswer.tsx
+++ app/chat/_components/ChatAnswer.tsx
@@ -2,6 +2,8 @@ import { VscCopy, VscRefresh, VscSave } from 'react-icons/vsc';
 
 import ButtonSquare from '@/app/_components/ButtonSquare';
 import ChatSource from './ChatSource';
+import ReactMarkdown from 'react-markdown';
+import { useToast } from '@/app/_hooks/useToast';
 
 interface ChatAnswerProps {
   text: string;
@@ -16,10 +18,32 @@ const ChatAnswer = ({
   isGenerating,
   sources = [],
 }: ChatAnswerProps) => {
+  const { showToast } = useToast();
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        showToast({
+          message: 'Answer copied to clipboard',
+          type: 'success',
+          duration: 3000,
+        });
+      })
+      .catch(() => {
+        showToast({
+          message: 'Failed to copy to clipboard',
+          type: 'error',
+          duration: 3000,
+        });
+      });
+  };
   return text.length ? (
     <div className='flex w-full max-w-2xl flex-col gap-4'>
       <div className='flex flex-col gap-4 rounded-box p-4'>
-        <div className='prose'>{text}</div>
+        <div className='prose prose-pre:whitespace-pre-wrap'>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </div>
         {(!isGenerating || !isCurrent) && sources.length > 0 && (
           <div className='flex flex-1 flex-wrap justify-end gap-2'>
             {sources.map((source, index) => (
@@ -30,7 +54,11 @@ const ChatAnswer = ({
       </div>
       {(!isGenerating || !isCurrent) && (
         <div className='flex w-full gap-2'>
-          <ButtonSquare size='xs' className='opacity-75 hover:opacity-100'>
+          <ButtonSquare
+            size='xs'
+            className='opacity-75 hover:opacity-100'
+            onClick={copyToClipboard}
+          >
             <VscCopy className='h-4 w-4' />
           </ButtonSquare>
           <ButtonSquare size='xs' className='opacity-75 hover:opacity-100'>
